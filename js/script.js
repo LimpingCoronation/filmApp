276c2d6ab85b66a174cc4277b6afb5557850e315
LimpingCoronation <as.voronin8@gmail.com>
2022-05-22 20:35:43 +0300

    Add interface

--- js/script.js
+++ js/script.js
@@ -1,70 +1,39 @@
 "use strict";
 
 let personalMovieDB = {
-    count: null,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    numberOfFilms: null,
-    getCountOfFilms: function() {
-        while ( this.count == null || this.count == '' || isNaN(this.count) ) {
-            this.count = +prompt("Сколько фильмов вы уже посмотрели?");
-        }
-    },
-    detectPersonalLevel: function() {
-        if (this.count < 10) {
-            alert("Просмотрено довольно мало филмов");
-        } 
-        else if (this.count >= 10 && this.count <= 30) {
-            alert("Вы классический зритель");
-        }
-        else if (this.count > 30) {
-            alert("Вы киноман!");
-        }
-        else {
-            alert("Произошла ошибка");
-        }
-    },
-    rememberFilms: function() {
-        for (let i = 0; i <= 1; i++) {
-            let getFilm = prompt("Один из последних просмотренных филмов?"),
-                getResume = +prompt("На сколько оцените его?");
-            
-            while (((getFilm.length >= 50 || getFilm.length == 0) || getFilm == null || getResume == null ||
-             getResume > 10 || getResume <= 0 ||
-             getResume == '' || getFilm == '') == true) {
-                getFilm = prompt("Один из последних просмотренных филмов?");
-                getResume = +prompt("На сколько оцените его?");
-            }
-            this.movies[`${getFilm}`] = `${getResume}`;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i < 4; i++) {
-            let genre;
-            while ( genre == null || genre == '' ) {
-                genre = prompt(`Ваш любимый жанр под номером ${i}:`);
-            }
-            this.genres.push(genre);
-        }
-        this.genres.forEach(function(item, i) {
-            console.log(`Любимый жанр #${i} - это ${item}`);
-        });
-    },
-    showMyDB: function() {
-        if ( !this.privat ) {
-            console.log(this);
-        }
-    },
-    toggleVisibleMyDB: function() {
-        ( this.privat == false ) ? this.privat = true : this.privat = false
-    },
+    movies: [
+        'Логан',
+        'Лига справедливости',
+        'Ла-ла лэнд',
+        'Одержимость',
+        'Скотт пилигрим против...'
+    ],
 }
 
-// personalMovieDB.getCountOfFilms();
-// personalMovieDB.detectPersonalLevel();
-// personalMovieDB.rememberFilms();
-// personalMovieDB.writeYourGenres();
-// personalMovieDB.toggleVisibleMyDB();
-// personalMovieDB.showMyDB();
+const advs = document.querySelectorAll(".promo__adv img"),
+    title = document.querySelector(".promo__genre"),
+    promoBG = document.querySelector (".promo__bg"),
+    promoInteractive = document.querySelector(".promo__interactive-list"),
+    promoInteractiveItems = document.querySelectorAll(".promo__interactive-item");
+
+advs.forEach(item => {
+    item.remove();
+});
+
+title.innerText = "драма";
+
+promoBG.style.cssText = 'background: url("../img/bg.jpg") center (center / cover) no-repeat';
+
+for ( let item of promoInteractiveItems ) {
+    item.remove();
+}
+
+personalMovieDB.movies.sort().forEach(function(item, i) {
+    let elem = document.createElement("li");
+    let delElem = document.createElement("div");
+    delElem.classList.add("delete");
+    elem.classList.add("promo__interactive-item");
+    promoInteractive.append(elem);
+    elem.innerText = `${ i + 1 }.${item}`;
+    elem.append(delElem);
+});
