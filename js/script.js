f7142a90207a3edfd60bfaecf80d7c9817403149
LimpingCoronation <as.voronin8@gmail.com>
2022-05-22 13:15:48 +0300

    code optimization. Add showMyDb func and condition for personalMovieDB.privat

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms;
 
-if (numberOfFilms < 10) {
-    alert("Просмотрено довольно мало филмов");
-} 
-else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
-    alert("Вы классический зритель");
+function getCountOfFilms() {
+    while ( numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms) ) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+    }
 }
-else if (numberOfFilms > 30) {
-    alert("Вы киноман!");
+
+getCountOfFilms() ;
+
+function detectPersonalLevel() {
+    if (numberOfFilms < 10) {
+        alert("Просмотрено довольно мало филмов");
+    } 
+    else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+        alert("Вы классический зритель");
+    }
+    else if (numberOfFilms > 30) {
+        alert("Вы киноман!");
+    }
+    else {
+        alert("Произошла ошибка");
+    }
 }
-else {
-    alert("Произошла ошибка");
-}
+
+detectPersonalLevel();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -25,14 +37,39 @@ let personalMovieDB = {
 
 let movies = {}
 
-for (let i = 0; i <= 1; i++) {
-    let getFilm = prompt("Один из последних просмотренных филмов?"),
-        getResume = +prompt("На сколько оцените его?");
-    
-    while (getFilm.length >= 50 || getFilm.length == 0) {
-        getFilm = prompt("Один из последних просмотренных филмов?");
+function rememberFilms() {
+    for (let i = 0; i <= 1; i++) {
+        let getFilm = prompt("Один из последних просмотренных филмов?"),
+            getResume = +prompt("На сколько оцените его?");
+        
+        while (((getFilm.length >= 50 || getFilm.length == 0) || getFilm == null || getResume == null ||
+         getResume > 10 || getResume <= 0 ||
+         getResume == '' || getFilm == '') == true) {
+            getFilm = prompt("Один из последних просмотренных филмов?");
+            getResume = +prompt("На сколько оцените его?");
+        }
+        personalMovieDB.movies[`${getFilm}`] = `${getResume}`;
     }
-    personalMovieDB.movies[`${getFilm}`] = `${getResume}`;
 }
 
-console.log(personalMovieDB);
+rememberFilms();
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        let genre;
+        while ( genre == null || genre == '' ) {
+            genre = prompt(`Ваш любимый жанр под номером ${i}:`);
+        }
+        personalMovieDB.genres.push(genre);
+    }
+}
+
+writeYourGenres();
+
+function showMyDB() {
+    if ( !personalMovieDB.privat ) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB()
